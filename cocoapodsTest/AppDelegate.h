81d762d46d61ed503d1ed96bb430edfd27d051bf
Stephen Kopylov <sk@pandra.ru>
2014-11-04 01:37:24 +0400

    testCommit

---
+++ cocoapodsTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  cocoapodsTest
+//
+//  Created by Admin on 04.11.14.
+//  Copyright (c) 2014 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
