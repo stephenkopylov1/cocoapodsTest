3b3a99dcc00e511b1c46f1682c42e68883cb4b6d
Stephen Kopylov <sk@pandra.ru>
2014-11-08 11:10:11 +0400

    no message

--- cocoapodsTest/cocoapodsAppDelegate.h
+++ cocoapodsTest/cocoapodsAppDelegate.h
@@ -8,7 +8,7 @@
 
 #import <UIKit/UIKit.h>
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate>
+@interface cocoapodsAppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
 
--- cocoapodsTest/cocoapodsAppDelegate.m
+++ cocoapodsTest/cocoapodsAppDelegate.m
@@ -6,14 +6,14 @@
 //  Copyright (c) 2014 Admin. All rights reserved.
 //
 
-#import "AppDelegate.h"
+#import "cocoapodsAppDelegate.h"
 #import "innerViewController.h"
 
-@interface AppDelegate () <UISplitViewControllerDelegate>
+@interface cocoapodsAppDelegate () <UISplitViewControllerDelegate>
 
 @end
 
-@implementation AppDelegate
+@implementation cocoapodsAppDelegate
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
